b271c92f6226cd10134b8c6ee638e58dcc832c6b
Andy <94316446+beersandrew@users.noreply.github.com>
2023-03-20 10:29:07 -0400

    Initial commit

---
+++ TestTeamCity/Program.cs
@@ -0,0 +1,10 @@
+using TestTeamCity;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
